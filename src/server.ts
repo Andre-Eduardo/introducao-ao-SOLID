b9a1d1b0ecf0add0c7a533c8df4dd26758bfdcd1
Andre <andre.eduardo11@hotmail.com>
2022-01-29 19:16:35 -0300

    adicionando requisiçoes swagger

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import swaggerUi from "swagger-ui-express";
 import { app } from ".";
 import swaggerFile from "./swagger.json";
 
-app.use("api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.listen(3333);
